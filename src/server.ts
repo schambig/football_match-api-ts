95e8e5f36f5d4e9ee9d6c94e634280eb3183ff4e
Salomon Chambi <salomonchambi@gmail.com>
2023-02-08 14:41:10 -0500

    feat: create a server using express

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+export default function createServer() {
+  const app = express();
+
+  app.get('/results/:team1/:team2', (req, res) => {
+    res.send(`Results from ${req.params.team1} vs ${req.params.team2}`);
+  });
+
+  return app;
+}
