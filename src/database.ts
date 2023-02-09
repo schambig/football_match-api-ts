2a401d4170f18b0e71b8605d47be0a04065040d0
Salomon Chambi <salomonchambi@gmail.com>
2023-02-09 01:56:46 -0500

    feat: implement function to get the clubs name and its score

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,29 @@
-import { Clubs, Matches } from "./types";
+import { Club, Clubs, Match, Matches } from "./types";
 
 const clubs: Clubs = require("../data/clubs.json");
 const results: Matches = require("../data/results.json");
 
+function getClub(code: string): string {
+  // return the value of the first element in the array where predicate is true, and undefined otherwise
+  const club = clubs.clubs.find(club => club.code === code)
+  if (!club) {
+    throw new Error("Team does't exist");
+  }
+  return club.name;
+};
+
+function getResult(team1: string, team2: string): Match {
+  // return the value of the first element in the array where predicate is true, and undefined otherwise
+  const match = results.matches.find(match => {
+    return team1 === match.team1 && team2 === match.team2
+  })
+  if (!match) {
+    throw new Error("This match hasn't been played");
+  }
+  return match;
+}
+
+export {
+  getClub,
+  getResult,
+}
