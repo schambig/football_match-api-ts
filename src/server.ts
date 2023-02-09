13e1e8dc17074c5a9955f8535ddbbb497936dbef
Salomon Chambi <salomonchambi@gmail.com>
2023-02-09 01:55:02 -0500

    feat: use try/catch to make the resquest to the database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,29 @@
 import express from 'express';
+import { getClub, getResult } from './database';
 
 export default function createServer() {
   const app = express();
 
   app.get('/results/:team1/:team2', (req, res) => {
-    res.send(`Results from ${req.params.team1} vs ${req.params.team2}`);
+    try {
+      let team1 = getClub(req.params.team1);
+      let team2 = getClub(req.params.team2);
+      let match = getResult(team1, team2);
+      if (match.score) {
+        res.json({
+          date: match.date,
+          result: `${team1} ${match.score.ft[0]} - ${match.score.ft[1]} ${team2}`
+        })
+      } else {
+          res.json({
+            date: match.date
+        });
+      }
+    } catch (e: any) {
+      res.status(400).json({
+        error: e.message
+      });
+    }
   });
 
   return app;
